ecdfa0f25e56cbd3c30e686964ad21b4d4c2fd7a
susu <susu_mm@126.com>
2016-02-25 10:46:19 +0800

    Initial Commit

---
+++ Simulation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Simulation
+//
+//  Created by su on 16/2/25.
+//  Copyright © 2016年 su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Simulation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Simulation
+//
+//  Created by su on 16/2/25.
+//  Copyright © 2016年 su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
